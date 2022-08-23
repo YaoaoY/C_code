501c32deb5b8bcb3b6c909319e17d32200571aab
Yaoyao2024 <3051337060@qq.com>
2022-08-23 19:42:58 +0800

    IF语句1

---
+++ Project2/Project2/源.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+# include <stdio.h>
+
+int main() {
+
+	int choice=0;
+	scanf("%d\n",&choice);
+
+	if (choice == 1) {
+		printf("好运藏在努力里\n");
+	}else if (choice == 2) {
+		printf("努力让自己变得更优秀然后骄傲的生活\n");
+	}
+	else if (choice == 3) {
+		printf("我们都在奔赴各自不同的人生\n");
+
+	}
+	else {
+		printf("迟到的向日葵也会开花\n");
+	}
+	return 0;
+}
