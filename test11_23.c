cb267d88e23e1c15e96bde2dc00c03d4cc12ab8c
Yaoyao2024 <3051337060@qq.com>
2022-11-27 10:36:13 +0800

    hdu1026(未通过）

--- test11_23.c
+++ test11_23.c
@@ -13,6 +13,47 @@ void Print(char* str)
     {
         do//if成立就直接开始反转，这个操作无条件执行一次，所以用do_while循环
         {
+#include <iostream>
+#include<bits/stdc++.h>
+            using namespace std;
+
+            int main()
+            {
+                int a = 0;
+                int b = 0;
+                int arr[600001] = { 0 };
+                scanf("%d %d", &a, &b);
+                int j = a;
+                for (int i = 0; i < b - a + 1; i++)
+                {
+                    arr[i] = j++;
+                }
+                int arr2[600001] = { 0 };
+                for (int i = 0; i < b - a; i++)
+                {
+                    scanf("%d", &arr2[i]);
+                }
+                int flag = 0;
+                for (int i = 0; i < b - a + 1; i++)
+                {
+                    flag = 0;
+                    for (int j = 0; j < b - a; j++)
+                    {
+                        if (arr[i] == arr2[j])
+                        {
+                            flag = 1;
+                            break;
+                        }
+                    }
+                    if (flag == 0)
+                    {
+                        printf("%d", arr[i]);
+                    }
+                }
+
+                return 0;
+            }
+
             char* left = p;//利用双指针，进行常规的字符串反转操作
             char* right = p + strlen(p) - 1;
             char temp = 0;//abc
